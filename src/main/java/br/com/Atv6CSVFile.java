5e2dd07be94d2aff2f1963d11c4b2318ed0675a8
anaoj <joohors0@gmail.com>
2024-04-18 11:19:44 -0300

    first commit

---
+++ src/main/java/br/com/Atv6CSVFile.java
@@ -0,0 +1,26 @@
+package br.com;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Atv6CSVFile {
+    public static void main(String[] args) {
+        try {
+            File myFile = new File("dados.csv");
+            Scanner myReader = new Scanner(myFile);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                String[] fields = data.split(",");
+                for (String field : fields) {
+                    System.out.print(field + " ");
+                }
+                System.out.println();
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
